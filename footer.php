3aca435e7673745ce95b38e281225c388ca1cd45
Chris Van Patten <chris@vanpattenmedia.com>
2012-11-06 09:23:37 -0500

    Use admin_url() properly in footer.php

--- footer.php
+++ footer.php
@@ -7,6 +7,6 @@
  * @since 1.0
  */
 function my_admin_footer() {
-	echo 'Built by <a href="#">My Company</a> with <a href="http://wordpress.org">WordPress</a>. &bull; <a href="' . admin_url() . 'freedoms.php">Freedoms</a> &bull; <a href="' . admin_url() . 'credits.php">Credits</a>';
+	echo 'Built by <a href="#">My Company</a> with <a href="http://wordpress.org">WordPress</a>. &bull; <a href="' . admin_url('freedoms.php') . '">Freedoms</a> &bull; <a href="' . admin_url('credits.php') . '">Credits</a>';
 }
 add_filter('admin_footer_text', 'my_admin_footer');
