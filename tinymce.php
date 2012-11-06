6aad732e71491907a61a7e4c2b2ca1ffbd3c81d1
Chris Van Patten <chris@vanpattenmedia.com>
2012-11-06 09:32:19 -0500

    Fix some indentation/spacing

--- tinymce.php
+++ tinymce.php
@@ -13,22 +13,23 @@
  * @since 1.0
  */
 function my_mce_buttons_1( $buttons ) {
-        return array(
-                'styleselect', 'formatselect', 'separator',
-                'bold', 'italic', 'separator',
-                'bullist', 'numlist', 'separator',
-                'link', 'unlink', 'separator',
-                'undo', 'redo', 'removeformat', 'separator',
-                'wp_more'
-        );
-        /* WordPress Default
-        return array(
-                'bold', 'italic', 'strikethrough', 'separator',
-                'bullist', 'numlist', 'blockquote', 'separator',
-                'justifyleft', 'justifycenter', 'justifyright', 'separator',
-                'link', 'unlink', 'wp_more', 'separator',
-                'spellchecker', 'fullscreen', 'wp_adv'
-        ); */
+	return array(
+		'styleselect', 'formatselect', 'separator',
+		'bold', 'italic', 'separator',
+		'bullist', 'numlist', 'separator',
+		'link', 'unlink', 'separator',
+		'undo', 'redo', 'removeformat', 'separator',
+		'wp_more'
+	);
+
+	/* WordPress Default
+	return array(
+		'bold', 'italic', 'strikethrough', 'separator',
+		'bullist', 'numlist', 'blockquote', 'separator',
+		'justifyleft', 'justifycenter', 'justifyright', 'separator',
+		'link', 'unlink', 'wp_more', 'separator',
+		'spellchecker', 'fullscreen', 'wp_adv'
+	); */
 }
 add_filter('mce_buttons', 'my_mce_buttons_1', 0);
 
@@ -36,20 +37,21 @@ add_filter('mce_buttons', 'my_mce_buttons_1', 0);
  * function my_mce_buttons_2
  * Set the buttons for the second row in TinyMCE
  *
- * This example disables the second row. The WordPress
+ * This example shows how to disable the second row. The WordPress
  * defaults are in a comment block below.
  *
  * @since 1.0
  */
 function my_mce_buttons_2( $buttons ) {
-        return array();
-        /* WordPress default
-        return array(
-                'formatselect', 'underline', 'justifyfull', 'forecolor', 'separator',
-                'pastetext', 'pasteword', 'removeformat', 'separator',
-                'media', 'charmap', 'separator',
-                'outdent', 'indent', 'separator',
-                'undo', 'redo', 'wp_help', 'styleselect'
-        ); */
+	return array();
+
+	/* WordPress default
+	return array(
+		'formatselect', 'underline', 'justifyfull', 'forecolor', 'separator',
+		'pastetext', 'pasteword', 'removeformat', 'separator',
+		'media', 'charmap', 'separator',
+		'outdent', 'indent', 'separator',
+		'undo', 'redo', 'wp_help', 'styleselect'
+	); */
 }
 add_filter('mce_buttons_2', 'my_mce_buttons_2', 0);
